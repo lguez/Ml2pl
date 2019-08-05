45edacd4e49b3974036132f8d1c627102e8180f8
Lionel GUEZ <guez@lmd.ens.fr>
2019-08-05 14:46:17 +0200

    Initiate version control.

---
+++ ml2pl.f90
@@ -0,0 +1,262 @@
+PROGRAM ml2pl
+
+  ! This is a program in Fortran 2003.
+  ! Author: Lionel GUEZ
+  ! See general description in the wrapper script.
+
+  use netcdf95, only: handle_err, nf95_close, nf95_copy_att, &
+       nf95_create, nf95_def_dim, nf95_def_var, nf95_enddef, &
+       nf95_get_att, nf95_get_var, nf95_gw_var, nf95_inq_dimid, &
+       nf95_inq_varid, nf95_inquire_dimension, nf95_open, &
+       nf95_put_att, nf95_put_var, find_coord, nf95_inquire_variable
+  use netcdf, only: nf90_clobber, nf90_double, nf90_float, nf90_global, &
+       nf90_max_name, nf90_nowrite, nf90_unlimited, NF90_FILL_REAL
+  use jumble, only: read_column
+  use numer_rec_95, only: regr1_lint, hunt
+  use nr_util, only: assert
+
+  IMPLICIT NONE
+
+  INTEGER  iim, n_lat, llm ! dimensions des données avant interpolation
+  INTEGER ntim ! nombre de dates
+
+  INTEGER n_plev ! nombre de niveaux de pression en sortie
+
+  REAL, allocatable:: pres(:, :, :) ! (iim, n_lat, llm)
+  ! input pressure field, in hPa 
+
+  REAL, allocatable:: ap(:) ! (llm)
+  REAL, allocatable:: b(:) ! (llm)
+  REAL, allocatable:: ps(:, :) ! (iim, n_lat) surface pressure field, in hPa
+
+  character(len=10) units
+
+  logical hybrid ! pressure is given through ap, b and ps 
+
+  REAL, allocatable:: rlon(:) ! (iim)
+  REAL, allocatable:: rlat(:) ! (n_lat)
+  double precision, allocatable:: time(:) ! (ntim)
+
+  integer i, j, k, l, n
+  integer n_var ! number of variables to interpolate
+
+  ! For NetCDF:
+  INTEGER dim_x, dim_y, dim_z, dim_t
+  integer, allocatable:: dimids(:)
+  INTEGER ncid_in, ncid_out, ncerr
+  integer varid_x, varid_y, varid_z, varid_t, varid_t_in, varid
+
+  integer, allocatable:: varid_in(:) ! (n_var)
+  ! IDs in the input NetCDF file of the variables to interpolate
+
+  integer, allocatable:: varid_out(:) ! (n_var)
+  ! IDs in the output NetCDF file of the interpolated variables
+
+  CHARACTER(len=nf90_max_name), allocatable:: varpossib(:) ! (n_var)
+  ! names of the NetCDF variables we want to interpolate
+
+  integer nv, nw
+  ! Number of variables to extrapolate and to set to 0 below
+  ! surface. Other variables are set to missing below
+  ! surface. Variable are in the order: extrapolated, set to 0, set to
+  ! missing, in "varpossib".
+
+  CHARACTER(len=nf90_max_name) pressure_var
+
+  REAL, allocatable:: var_ml(:, :, :, :) ! (iim, n_lat, llm, n_var)
+  ! variables at model levels
+
+  REAL, allocatable:: var_pl(:, :, :, :) ! (iim, n_lat, n_plev, n_var)
+  ! variables at pressure levels
+
+  integer surf_loc ! location of surface in target pressure levels
+
+  REAL, allocatable:: plev(:) ! (n_plev)
+  ! target pressure levels, in hPa, in descending order
+
+  !---------------------------------------------------------------------
+
+  ! Read the names of the variables:
+  call read_column("variable_list_ml2pl", varpossib)
+  n_var =size(varpossib)
+
+  ! Read target pressure levels:
+  call read_column("press_levels.txt", plev, first=2)
+  n_plev = size(plev)
+  ! Quick check:
+  if (n_plev >= 2) &
+       call assert(plev(1) > plev(2), "plev should be in descending order")
+
+  read *, nv, nw, pressure_var
+
+  call nf95_open("input_file_ml2pl.nc", nf90_nowrite, ncid_in)
+
+  ! Read horizontal coordinates:
+
+  call find_coord(ncid_in, varid=varid, std_name="longitude")
+  call nf95_gw_var(ncid_in, varid, rlon)
+  iim = size(rlon)
+  print *, 'ml2pl: iim = ', iim
+
+  call find_coord(ncid_in, varid=varid, std_name="latitude")
+  call nf95_gw_var(ncid_in, varid, rlat)
+  n_lat = size(rlat)
+  print *, 'ml2pl: n_lat = ', n_lat
+
+  ! Read IDs of variables to interpolate:
+  allocate(varid_in(n_var))
+  do n = 1, n_var
+     call nf95_inq_varid(ncid_in, trim(varpossib(n)), varid_in(n))
+  end do
+
+  ! Get the number of model levels:
+  call nf95_inquire_variable(ncid_in, varid_in(1), dimids=dimids)
+  call nf95_inquire_dimension(ncid_in, dimids(3), nclen=llm)
+  print *, 'ml2pl: llm = ', llm
+
+  hybrid = len_trim(pressure_var) == 0
+
+  if (hybrid) then
+     print *, 'Using "ap", "b" and "ps" for the input pressure field...'
+
+     call nf95_inq_varid(ncid_in, 'ap', varid)
+     call nf95_get_att(ncid_in, varid, "units", units)
+     call assert(units == "Pa", "ap should be in Pa")
+
+     allocate(ap(llm), b(llm), ps(iim, n_lat))
+
+     call nf95_get_var(ncid_in, varid, ap)
+     ap = ap / 100. ! convert from Pa to hPa
+
+     call nf95_inq_varid(ncid_in, 'b', varid)
+     call nf95_get_var(ncid_in, varid, b)
+
+     call nf95_inq_varid(ncid_in, 'ps', varid)
+  else
+     print *, 'Using "' // trim(pressure_var) // &
+          '" for the input pressure field...'
+     call nf95_inq_varid(ncid_in, trim(pressure_var), varid)
+
+     call nf95_get_att(ncid_in, varid, "units", units)
+     call assert(units == "Pa", trim(pressure_var) // " should be in Pa")
+  end if
+
+  ! Read time coordinate:
+  call find_coord(ncid_in, varid=varid_t_in, std_name="time")
+  if (varid_t_in == 0) then
+     print *, "ml2pl: could not find a time coordinate"
+     stop 1
+  end if
+  call nf95_gw_var(ncid_in, varid_t_in, time)
+  ntim = size(time)
+  print *, 'ml2pl: ntim = ', ntim
+
+  call nf95_create("output_file_ml2pl.nc", nf90_clobber, ncid_out)
+
+  call nf95_put_att(ncid_out, nf90_global, 'comment', &
+       'interpolated to pressure levels')
+  call nf95_def_dim(ncid_out, 'longitude', iim, dim_x)
+  call nf95_def_dim(ncid_out, 'latitude', n_lat, dim_y)
+  call nf95_def_dim(ncid_out, 'plev', n_plev, dim_z)
+  call nf95_def_dim(ncid_out, 'time', nf90_unlimited, dim_t)
+
+  call nf95_def_var(ncid_out, 'longitude', nf90_float, dim_x, varid_x)
+  call nf95_put_att(ncid_out, varid_x, 'standard_name', 'longitude')
+  call nf95_put_att(ncid_out, varid_x, 'units', 'degrees_east')
+
+  call nf95_def_var(ncid_out, 'latitude', nf90_float, dim_y, varid_y)
+  call nf95_put_att(ncid_out, varid_y, 'standard_name', 'latitude')
+  call nf95_put_att(ncid_out, varid_y, 'units', 'degrees_north')
+
+  call nf95_def_var(ncid_out, 'plev', nf90_float, dim_z, varid_z)
+  call nf95_put_att(ncid_out, varid_z, 'standard_name', 'air_pressure')
+  call nf95_put_att(ncid_out, varid_z, 'units', 'hPa')
+
+  call nf95_def_var(ncid_out, 'time', nf90_double, dim_t, varid_t)
+  call nf95_put_att(ncid_out, varid_t, 'standard_name', 'time')
+  call nf95_copy_att(ncid_in, varid_t_in, 'units', ncid_out, varid_t)
+  call nf95_copy_att(ncid_in, varid_t_in, 'calendar', ncid_out, varid_t)
+
+  ! Create interpolated variables:
+  allocate(varid_out(n_var))
+  do n = 1, n_var
+     call nf95_def_var(ncid_out, trim(varpossib(n)), nf90_float, &
+          (/dim_x, dim_y, dim_z, dim_t/), varid_out(n))
+     call nf95_copy_att(ncid_in, varid_in(n), 'units', ncid_out, &
+          varid_out(n), ncerr)
+     call nf95_put_att(ncid_out, varid_out(n), "_FillValue", NF90_FILL_REAL)
+  end do
+
+  call nf95_enddef(ncid_out)
+
+  ! Horizontal and time coordinates are the same in the input and output files:
+  call nf95_put_var(ncid_out, varid_x, rlon)
+  call nf95_put_var(ncid_out, varid_y, rlat)
+  call nf95_put_var(ncid_out, varid_t, time)
+
+  call nf95_put_var(ncid_out, varid_z, plev)
+
+  allocate(var_ml(iim, n_lat, llm, n_var), var_pl(iim, n_lat, n_plev, n_var))
+  allocate(pres(iim, n_lat, llm))
+
+  ! For each date, read the pressure field and all the variables to
+  ! interpolate, then interpolate at each horizontal position:
+  DO l = 1, ntim
+     if (hybrid) then
+        call nf95_get_var(ncid_in, varid, ps, start=(/1, 1, l/))
+        ps = ps / 100. ! convert from Pa to hPa
+
+        forall (k = 1: llm) pres(:, :, k) = ap(k) + b(k) * ps
+     else
+        call nf95_get_var(ncid_in, varid, pres, start=(/1, 1, 1, l/))
+        pres = pres / 100. ! convert from Pa to hPa
+     end if
+
+     ! Quick check:
+     call assert(pres(1, 1, 1) > pres(1, 1, 2), &
+          "Input pressure field should decrease with increasing level index")
+
+     do n = 1, n_var
+        call nf95_get_var(ncid_in, varid_in(n), var_ml(:, :, :, n), &
+             start=(/1, 1, 1, l/))
+     end do
+
+     if (nv >= 1) then
+        ! Variables extrapolated below surface
+        do j = 1, n_lat
+           do i = 1, iim
+              var_pl(i, j, :, :nv) = regr1_lint(var_ml(i, j, :, :nv), &
+                   xs = log(pres(i, j, :)), xt = log(plev))
+           end do
+        end do
+     end if
+
+     if (nv < n_var) then
+        ! Variables set to 0 or missing below surface
+        surf_loc = 1 ! first guess
+
+        do j = 1, n_lat
+           do i = 1, iim
+              call hunt(plev, pres(i, j, 1), surf_loc)
+              ! {plev(surf_loc + 1) <= pres(i, j, 1) <=  plev(surf_loc)}
+
+              var_pl(i, j, :surf_loc, nv + 1: nv + nw) = 0.
+              var_pl(i, j, :surf_loc, nv + nw + 1:) = NF90_FILL_REAL
+
+              var_pl(i, j, surf_loc + 1:, nv + 1:) &
+                   = regr1_lint(var_ml(i, j, :, nv + 1:), &
+                   xs = log(pres(i, j, :)), xt = log(plev(surf_loc + 1:)))
+           end do
+        end do
+     end if
+
+     do n = 1, n_var
+        call nf95_put_var(ncid_out, varid_out(n), var_pl(:, :, :, n), &
+             start=(/1, 1, 1, l/))
+     end DO
+  end do
+
+  call nf95_close(ncid_out)
+  call nf95_close(ncid_in)
+
+END PROGRAM ml2pl
---
+++ ml2pl.sh
@@ -0,0 +1,202 @@
+# This is a template. Replace the ellipses.
+
+#!/bin/bash
+
+# Author: Lionel GUEZ
+
+# This script is a wrapper for a Fortran program. For an explanation
+# of programming choices, see notes.
+
+# Do not run several instances of this script in parallel in the same
+# directory. File names are not made different for different
+# instances.
+
+##set -x
+
+# Absolute path to Fortran executable:
+executable=/.../build/Ml2pl/ml2pl
+
+# Set up the necessary environment:
+module purge 2>/dev/null
+module load netcdf... 2>/dev/null
+module load nco... 2>/dev/null
+module load intel... 2>/dev/null
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:...NetCDF...
+
+USAGE="usage: ml2pl.sh [OPTION]... input-file output-file [pressure-file]
+
+Interpolates NetCDF variables from model levels to pressure
+levels. The interpolation is linear in logarithm of pressure. The
+input variables depend on longitude, latitude, vertical level and
+time. There is no constraint on the dimensions.
+
+Options:
+   -h                       : this help message
+   -p variable              : name of 4-dimensional variable in the input file
+                              or the pressure file containing the
+                              pressure field at model levels
+   -v variable[,variable...]: names of variables you want to interpolate, 
+                              and possibly extrapolate if target pressure 
+                              level is below surface
+   -w variable[,variable...]: names of variables you want to interpolate, 
+                              or set to 0 if target pressure level is below
+                              surface
+   -m variable[,variable...]: names of variables you want to interpolate, 
+                              or set to missing if target pressure level is 
+                              below surface
+
+input-file, output-file and pressure-file are NetCDF files.
+
+You must list the variables you want to interpolate, each variable
+listed after either -v, -w or -m. There must be at least one variable
+listed, following either -v, -w or -m.
+
+The pressure field at model levels can be specified in input-file or
+pressure-file either through hybrid coefficients and surface pressure
+or directly from 4-dimensional pressure. In both cases, pressure must
+be given in Pa and decrease when the index of model level
+increases. This is checked quickly in the program. If option -p is not
+used then the program will look for \"ap\", \"bp\" (hybrid
+coefficients) and \"ps\" (surface pressure) in the input file or the
+pressure file.
+
+The target pressure levels should be in a text file called
+\"press_levels.txt\" in the current directory at run-time. The first
+line of the file is skipped, assuming it is a title line. Pressure
+levels should be in hPa, in descending order, one value per
+line. There is no constraint on these values nor on the number of
+values.
+
+For further documentation, see:
+http://lmdz.lmd.jussieu.fr/utilisateurs/outils/utilisation-de-lmdz#section-9"
+
+while getopts hp:v:w:m: name
+  do
+  case $name in
+      p) 
+	  pressure_var=$OPTARG;;
+      v) 
+	  variable_list_v=$OPTARG;;
+      w) 
+	  variable_list_w=$OPTARG;;
+      m) 
+	  variable_list_m=$OPTARG;;
+      h)
+	  echo "$USAGE" >&2
+	  exit;;
+      \?)
+	  echo "Use option -h for help"
+	  exit 1;;
+  esac
+done
+
+if [[ -n $pressure_var ]]
+then
+    # For Climaf, remove pressure_var from variable lists:
+
+    variable_list_v=`echo $variable_list_v | sed -e 's/'$pressure_var'//g' -e s'/,,*/,/g' -e s'/^,//' -e 's/,$//'`
+    
+    variable_list_w=`echo $variable_list_w | sed -e 's/'$pressure_var'//g' -e s'/,,*/,/g' -e s'/^,//' -e 's/,$//'`
+
+    variable_list_m=`echo $variable_list_m | sed -e 's/'$pressure_var'//g' -e s'/,,*/,/g' -e s'/^,//' -e 's/,$//'`
+fi
+
+if [[ -z $variable_list_v && -z $variable_list_w && -z $variable_list_m ]]
+    then
+    echo "Specify at least one variable, following -v, -w or -m."
+    echo "Use option -h for help"
+    exit 1
+fi
+
+shift $((OPTIND - 1))
+
+if (($# <= 1))
+then
+    echo "Missing input or output file."
+    echo "Use option -h for help"
+    exit 1
+elif (($# >= 4))
+then
+    echo "Too many arguments."
+    echo "Use option -h for help"
+    exit 1
+fi
+
+set -e
+
+if [[ ! -x $executable ]]
+    then
+    echo "$executable not found or not executable"
+    exit 1
+fi
+
+if [[ ! -f $1 ]]
+    then
+    echo "ml2pl.sh: $1 not found"
+    exit 1
+fi
+
+if [[ ! -f "press_levels.txt" ]]
+    then
+    echo "ml2pl.sh: press_levels.txt not found"
+    echo "Use option -h for help"
+    exit 1
+fi
+
+if (($# == 2))
+then
+    ln -sf $1 input_file_ml2pl.nc
+else
+    # $# == 3
+
+    if [[ ! -f $3 ]]
+    then
+	echo "ml2pl.sh: $3 not found"
+	exit 1
+    fi
+
+    cp $3 input_file_ml2pl.nc
+    ncks --append ${variable_list_v:+--variable=$variable_list_v} \
+	${variable_list_w:+--variable=$variable_list_w} \
+	${variable_list_m:+--variable=$variable_list_m} $1 input_file_ml2pl.nc
+    echo "Appended $1 to $3."
+fi
+
+output_file=$2
+
+IFS=","
+
+if [[ -z $variable_list_v ]]
+then
+    nv=0
+else
+    set $variable_list_v
+    nv=$#
+fi
+
+if [[ -z $variable_list_w ]]
+then
+    nw=0
+else
+    set $variable_list_w
+    nw=$#
+fi
+
+# Create the list of variables:
+set $variable_list_v $variable_list_w $variable_list_m
+for my_var in $*
+  do
+  echo $my_var
+done >variable_list_ml2pl
+
+# Run the Fortran program:
+$executable <<EOF
+$nv
+$nw
+"$pressure_var"
+EOF
+# (Quotes around $pressure_var are necessary for the case when
+# pressure_var is not defined.)
+
+mv output_file_ml2pl.nc $output_file
+rm input_file_ml2pl.nc variable_list_ml2pl
